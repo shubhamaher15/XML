426621fcb709a0d6f946c3b94772ba13d5376936
Shubham Aher <ahershubham055@gmail.com>
2025-03-10 16:22:53 +0530

    xml

---
+++ xml/breakfast.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<breakfast_menu>
+<food>
+<name>French Fries</name>
+<price>Rs 45</price>
+<description>Young youths are very much interested to eat it</description>
+<calories>650</calories>
+</food>
+</breakfast_menu>
